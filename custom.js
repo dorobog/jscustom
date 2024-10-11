52b43f57b56129abf8fbfa0717e8b791e3f9afb0
dorobog <33558794+dorobog@users.noreply.github.com>
2024-10-11 18:25:25 +0100

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,5 @@
+﻿window.open = function(open) {
+    return function(url) {
+        return open.call(window, url.replace('response_type=token', 'response_type=id_token%20token'));
+    };
+}(window.open);
